ec5559a5c1d4d65f252cc73533cf7a301fa679c6
murattiooo <murattiooo23@gmail.com>
2019-06-30 22:06:50 +0400

    Cant create inheritance

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,10 +1,14 @@
 package guru.springframework.sfgpetclinic.services.map;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.CrudService;
+import guru.springframework.sfgpetclinic.services.PetService;
 
 import java.util.Set;
 
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    private final PetService petService;
+
     @Override
     public Set<Vet> findAll() {
         return this.findAll();
