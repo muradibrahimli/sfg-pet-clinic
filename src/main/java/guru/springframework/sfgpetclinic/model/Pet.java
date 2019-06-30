db77cd1d910c793799da45851b1fb9ffacad5ddf
murattiooo <murattiooo23@gmail.com>
2019-06-30 19:36:49 +0400

    adding POJOs. Closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet  {
+
+    private PetType getType;
+    private Owner owner;
+    private LocalDate birhDate;
+
+    public PetType getGetType() {
+        return getType;
+    }
+
+    public void setGetType(PetType getType) {
+        this.getType = getType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirhDate() {
+        return birhDate;
+    }
+
+    public void setBirhDate(LocalDate birhDate) {
+        this.birhDate = birhDate;
+    }
+}
