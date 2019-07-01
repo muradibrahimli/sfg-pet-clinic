f233b7159fcb77de3746c6c4510391ebb45ab7d0
murattiooo <murattiooo23@gmail.com>
2019-07-01 11:11:09 +0400

    Refactored Map Impl to abstract ID generation. Closes #23

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataLoader implements CommandLineRunner {
 
-     private final OwnerService ownerService;
-     private final VetService vetService;
+    private final OwnerService ownerService;
+    private final VetService vetService;
 
     public DataLoader(OwnerService ownerService, VetService vetService) {
         this.ownerService = ownerService;
@@ -21,34 +21,26 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Owner owner1=new Owner();
-        owner1.setId(1L);
+        Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
-
         ownerService.save(owner1);
 
-        Owner owner2= new Owner();
-        owner2.setId(2L);
+        Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
-
         ownerService.save(owner2);
 
         System.out.println("Loaded Owners...");
 
-        Vet vet1=new Vet();
-        vet1.setId(1L);
+        Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
-
         vetService.save(vet1);
 
-        Vet vet2=new Vet();
-        vet2.setId(2L);
+        Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
-
         vetService.save(vet2);
 
         System.out.println("Loaded Vets...");
