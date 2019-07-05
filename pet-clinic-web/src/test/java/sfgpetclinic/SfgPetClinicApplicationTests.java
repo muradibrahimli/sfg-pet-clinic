1ebc0812063422bc1fb99bc720cddd139fbe1ce3
murattiooo <murattiooo23@gmail.com>
2019-07-05 12:57:25 +0400

    added test. closes #49

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,13 +1,13 @@
 package sfgpetclinic;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testng.annotations.Test;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SfgPetClinicApplicationTests {
+public class  SfgPetClinicApplicationTests {
 
     @Test
     public void contextLoads() {
